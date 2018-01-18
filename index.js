e5356fce2a5ac29fd4aac9a1709e9d608fe61b47
Saturnin Pugnet <saturnin.pugnet@transferwise.com>
2018-01-18 19:43:08 +0100

    Testing db

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
+
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+}
+client.end();
+});
