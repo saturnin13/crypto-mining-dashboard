583a9e90d4e1628be74c59a8662e0d25a00df957
Saturnin Pugnet <saturnin.pugnet@transferwise.com>
2018-01-30 23:08:36 +0000

    login setup

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,10 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local'), (req, resp)=>{
+        log.debug(req.user);
+        resp.send(req.user);
+    });
 
     // =====================================
     // SIGNUP ==============================
