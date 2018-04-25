e981a36da346443c0e1ec9600764248e1baac4d5
Satrnin Pugnet <saturnin.pugnet14@ic.ac.uk>
2018-04-25 20:14:40 +0200

    Database has been updated

--- app/mydb.js
+++ app/mydb.js
@@ -2,7 +2,7 @@ var db;
 exports.db = function() {
     if (!db) {
         var pgp = require('pg-promise')(/*options*/);
-        db = pgp(process.env.DATABASE_URL || "postgres://lncgqbiyoivknm:16497d98c045a638262b080a515986d172cadc0799e23f7ebc1cd225556116a7@ec2-54-217-214-201.eu-west-1.compute.amazonaws.com:5432/dabhdlnlb316fm?ssl=true");
+        db = pgp(process.env.HEROKU_POSTGRESQL_MAROON_URL || "postgres://lmxhpacdmmgnfr:0f78fab407cdf1699b50b2fec55a742f65ab1a5cfbbb2c166394a09eb6acf652@ec2-54-247-89-189.eu-west-1.compute.amazonaws.com:5432/denvqvnkc5gm9j?ssl=true");
     }
     return db;
 }
--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-    connectionString: process.env.DATABASE_URL || "postgres://lncgqbiyoivknm:16497d98c045a638262b080a515986d172cadc0799e23f7ebc1cd225556116a7@ec2-54-217-214-201.eu-west-1.compute.amazonaws.com:5432/dabhdlnlb316fm",
+    connectionString: process.env.HEROKU_POSTGRESQL_MAROON_URL || "postgres://lmxhpacdmmgnfr:0f78fab407cdf1699b50b2fec55a742f65ab1a5cfbbb2c166394a09eb6acf652@ec2-54-247-89-189.eu-west-1.compute.amazonaws.com:5432/denvqvnkc5gm9j",
     ssl: true
 };
